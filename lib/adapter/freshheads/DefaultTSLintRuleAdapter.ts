a5d16708a4fba8efe4b8697facf6d017ae1568c0
Gijs Nieuwenhuis <gijs.nieuwenhuis@freshheads.com>
2019-05-02 11:43:22 +0200

    Added default freshheads adapter to add tslint-loader rule

---
+++ lib/adapter/freshheads/DefaultTSLintRuleAdapter.ts
@@ -0,0 +1,65 @@
+import { Adapter, NextCallback } from '../Adapter';
+import path from 'path';
+import { BuilderConfig } from '../../Builder';
+import { Configuration, RuleSetRule } from 'webpack';
+import { checkIfModuleIsInstalled } from '../../utility/moduleHelper';
+import webpack = require('webpack');
+
+export type Config = {
+    configurationPath: string;
+};
+
+export const DEFAULT_CONFIG: Config = {
+    configurationPath: path.resolve(process.cwd(), 'tslint.json'),
+};
+
+export default class DefaultTSLintRuleAdapter implements Adapter {
+    private config: Config;
+
+    constructor(config: Partial<Config> = {}) {
+        this.config = {
+            ...DEFAULT_CONFIG,
+            ...config,
+        };
+    }
+
+    public apply(
+        webpackConfig: Configuration,
+        builderConfig: BuilderConfig,
+        next: NextCallback
+    ) {
+        this.validateAllRequiredModulesAreInstalled();
+
+        const rule: RuleSetRule = {
+            test: /\.tsx?$/,
+            exclude: /node_modules/,
+            enforce: 'pre',
+            use: [
+                {
+                    loader: 'tslint-loader',
+                    options: {
+                        configFile: this.config.configurationPath,
+                    },
+                },
+            ],
+        };
+
+        if (typeof webpackConfig.module === 'undefined') {
+            webpackConfig.module = {
+                rules: [],
+            };
+        }
+
+        webpackConfig.module.rules.push(rule);
+
+        next();
+    }
+
+    private validateAllRequiredModulesAreInstalled() {
+        if (!checkIfModuleIsInstalled('tslint-loader')) {
+            throw new Error(
+                "The 'tslint-loader'-module needs to be installed for loading images and fonts"
+            );
+        }
+    }
+}
