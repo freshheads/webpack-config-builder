7a1a7a0ced4f34d915aa3e1d0bbf7022c88baef7
remco-freshheads <remco.abalain@freshheads.com>
2019-05-31 16:02:19 +0200

    Revert "#4 dynamic import modules"
    This reverts commit 56bc32c9d4fb3cf039fbe4d22f66277373b3ce02.

--- lib/adapter/freshheads/DefaultPluginsAdapter.ts
+++ lib/adapter/freshheads/DefaultPluginsAdapter.ts
@@ -1,11 +1,16 @@
 import { Adapter, NextCallback } from '../Adapter';
 import { Configuration } from 'webpack';
 import Builder, { BuilderConfig, Environment } from '../../Builder';
-import {
+import DefaultCleanPluginAdapter from './DefaultCleanPluginAdapter';
+import DefaultStatsWriterPluginAdapter from './DefaultStatsWriterPluginAdapter';
+import DefaultDefinePluginAdapter from './DefaultDefinePluginAdapter';
+import DefaultProvidePluginAdapter from './DefaultProvidePluginAdapter';
+import DefaultCopyPluginAdapter, {
     Config as CopyConfig,
     DEFAULT_CONFIG as DEFAULT_COPY_CONFIG,
 } from './DefaultCopyPluginAdapter';
-import {
+import DefaultUglifyPluginAdapter from './DefaultUglifyPluginAdapter';
+import DefaultStylelintPluginAdapter, {
     Config as StylelintConfig,
     DEFAULT_CONFIG as DEFAULT_STYLELINT_CONFIG,
 } from './DefaultStylelintPluginAdapter';
@@ -68,48 +73,36 @@ export default class DefaultPluginsAdapter implements Adapter {
         const builder = new Builder(builderConfig, webpackConfig);
 
         if (this.config.clean.enabled) {
-            const DefaultCleanPluginAdapter = require('./DefaultCleanPluginAdapter');
-
-            builder.add(new DefaultCleanPluginAdapter.default());
+            builder.add(new DefaultCleanPluginAdapter());
         }
 
         if (this.config.statsWriter.enabled) {
-            const DefaultStatsWriterPluginAdapter = require('./DefaultStatsWriterPluginAdapter');
-
-            builder.add(new DefaultStatsWriterPluginAdapter.default());
+            builder.add(new DefaultStatsWriterPluginAdapter());
         }
 
         if (this.config.define.enabled) {
-            const DefaultDefinePluginAdapter = require('./DefaultDefinePluginAdapter');
-
-            builder.add(new DefaultDefinePluginAdapter.default());
+            builder.add(new DefaultDefinePluginAdapter());
         }
 
         if (this.config.provide.enabled) {
-            const DefaultProvidePluginAdapter = require('./DefaultProvidePluginAdapter');
-
-            builder.add(new DefaultProvidePluginAdapter.default());
+            builder.add(new DefaultProvidePluginAdapter());
         }
 
         if (this.config.copy.enabled) {
-            const DefaultCopyPluginAdapter = require('./DefaultCopyPluginAdapter');
-
-            builder.add(new DefaultCopyPluginAdapter.default(this.config.copy));
+            builder.add(new DefaultCopyPluginAdapter(this.config.copy));
         }
 
         const isProduction = builderConfig.env === Environment.Production;
 
         if (isProduction) {
             if (this.config.uglify.enabled) {
-                const DefaultUglifyPluginAdapter = require('./DefaultUglifyPluginAdapter');
-
-                builder.add(new DefaultUglifyPluginAdapter.default());
+                builder.add(new DefaultUglifyPluginAdapter());
             }
         } else {
             if (this.config.stylelint.enabled) {
-                const DefaultStylelintPluginAdapter = require('./DefaultStylelintPluginAdapter');
-
-                builder.add(new DefaultStylelintPluginAdapter.default(this.config.stylelint));
+                builder.add(
+                    new DefaultStylelintPluginAdapter(this.config.stylelint)
+                );
             }
         }
 
