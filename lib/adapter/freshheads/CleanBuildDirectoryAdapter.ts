579df3a4495b5333798ad3bdd55fa71545442346
Gijs Nieuwenhuis <gijs.nieuwenhuis@freshheads.com>
2019-05-31 11:40:53 +0200

    Refactor: rename clean build directory adapter to match usage instead of webpack config application (#3)

--- lib/adapter/freshheads/CleanBuildDirectoryAdapter.ts
+++ lib/adapter/freshheads/CleanBuildDirectoryAdapter.ts
@@ -3,7 +3,7 @@ import { Configuration, Plugin } from 'webpack';
 import { BuilderConfig } from '../../Builder';
 import CleanWebpackPlugin from 'clean-webpack-plugin';
 
-export default class DefaultCleanPluginAdapter implements Adapter {
+export default class CleanBuildDirectoryAdapter implements Adapter {
     public apply(
         webpackConfig: Configuration,
         builderConfig: BuilderConfig,
