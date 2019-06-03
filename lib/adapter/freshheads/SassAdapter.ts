afb228c8815673d591b1afd26bce7b24132a78f7
Gijs Nieuwenhuis <gijs.nieuwenhuis@freshheads.com>
2019-06-03 10:09:07 +0200

    #3 Refactor: Move setup for loading and linting sass to seperate adapters and call it in SassAdapter (SRP)

--- lib/adapter/freshheads/SassAdapter.ts
+++ lib/adapter/freshheads/SassAdapter.ts
@@ -1,31 +1,31 @@
-import { Configuration, RuleSetRule, RuleSetUseItem, Plugin } from 'webpack';
+import { Configuration } from 'webpack';
 import deepmerge from 'deepmerge';
 import { Adapter, NextCallback } from '../Adapter';
-import { BuilderConfig, Environment } from '../../Builder';
-import { checkIfModuleIsInstalled } from '../../utility/moduleHelper';
-import ExtractCssPluginAdapter from './ExtractCssPluginAdapter';
-import { Builder } from '../..';
-import path from 'path';
+import { BuilderConfig } from '../../Builder';
+import SassLintingAdapter, {
+    Config as LintingConfig,
+    DEFAULT_CONFIG as DEFAULT_LINTING_CONFIG,
+} from './SassLintingAdapter';
+import SassLoaderAdapter, {
+    Config as LoaderConfig,
+    DEFAULT_CONFIG as DEFAULT_LOADER_CONFIG,
+} from './SassLoaderAdapter';
+import Builder from '../../Builder';
+
+type EnabledConfig = {
+    enabled: boolean;
+};
 
 export type Config = {
-    cssLoaderOptions: { [key: string]: any };
-    sassLoaderOptions: { [key: string]: any };
-    linting: {
-        enabled: boolean;
-        configurationPath: string;
-    };
+    loader: LoaderConfig;
+    linting: EnabledConfig & LintingConfig;
 };
 
 export const DEFAULT_CONFIG: Config = {
-    cssLoaderOptions: {
-        sourceMap: true,
-    },
-    sassLoaderOptions: {
-        sourceMap: true,
-    },
+    loader: DEFAULT_LOADER_CONFIG,
     linting: {
         enabled: true,
-        configurationPath: path.resolve(process.cwd(), '.stylelintrc'),
+        ...DEFAULT_LINTING_CONFIG,
     },
 };
 
@@ -41,140 +41,16 @@ export default class SassAdapter implements Adapter {
         builderConfig: BuilderConfig,
         next: NextCallback
     ) {
-        this.validateAllRequiredModulesAreInstalled();
+        const builder = new Builder(builderConfig, webpackConfig);
 
-        if (typeof webpackConfig.module === 'undefined') {
-            webpackConfig.module = { rules: [] };
+        builder.add(new SassLoaderAdapter(this.config.loader));
+
+        if (this.config.linting.enabled) {
+            builder.add(new SassLintingAdapter(this.config.linting));
         }
 
-        const isProduction = builderConfig.env === Environment.Production;
-
-        webpackConfig.module.rules.push(this.createLoaderRule(isProduction));
-
-        if (this.config.linting.enabled && !isProduction) {
-            this.validateAllLintingModulesAreInstalled();
-
-            if (typeof webpackConfig.plugins === 'undefined') {
-                webpackConfig.plugins = [];
-            }
-
-            webpackConfig.plugins.push(this.createLintingPlugin());
-        }
+        builder.build();
 
         next();
-
-        if (!this.checkMiniCssExtractPluginIsInWebpackConfig(webpackConfig)) {
-            const internalBuilder = new Builder(builderConfig, webpackConfig);
-            internalBuilder.add(new ExtractCssPluginAdapter());
-
-            internalBuilder.build();
-        }
-    }
-
-    private createLintingPlugin(): Plugin {
-        const StylelintBarePlugin = require('stylelint-bare-webpack-plugin');
-
-        return new StylelintBarePlugin({
-            configFile: this.config.linting.configurationPath,
-            files: path.resolve(process.cwd(), 'src/scss/**/*.s?(c|a)ss'),
-        });
-    }
-
-    private createLoaderRule(isProduction: boolean): RuleSetRule {
-        return {
-            test: /\.scss$/,
-            use: [
-                {
-                    loader: require('mini-css-extract-plugin').loader,
-                },
-                {
-                    loader: 'css-loader',
-                    options: this.config.cssLoaderOptions,
-                },
-                this.createPostCssLoaderUse(isProduction),
-                {
-                    loader: 'resolve-url-loader',
-                    options: {
-                        sourceMap: true,
-                    },
-                },
-                {
-                    loader: 'sass-loader',
-                    options: this.config.sassLoaderOptions,
-                },
-            ],
-        };
-    }
-
-    private checkMiniCssExtractPluginIsInWebpackConfig(
-        webpackConfig: Configuration
-    ): boolean {
-        if (typeof webpackConfig.plugins === 'undefined') {
-            return false;
-        }
-
-        const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
-        const plugins = webpackConfig.plugins;
-
-        return (
-            plugins.findIndex(
-                plugin => plugin instanceof MiniCssExtractPlugin
-            ) !== -1
-        );
-    }
-
-    private validateAllLintingModulesAreInstalled() {
-        if (!checkIfModuleIsInstalled('stylelint')) {
-            throw new Error(
-                "Expecting module 'stylelint' to be installed as it is required for this adapter to work"
-            );
-        }
-    }
-
-    private validateAllRequiredModulesAreInstalled() {
-        const requiredModules = [
-            'mini-css-extract-plugin',
-            'autoprefixer',
-            'sass-loader',
-            'resolve-url-loader',
-            'css-loader',
-            'postcss-loader',
-        ];
-
-        requiredModules.forEach(module => {
-            if (!checkIfModuleIsInstalled(module)) {
-                throw new Error(
-                    `The '${module}' needs to be installed for this adapter to work`
-                );
-            }
-        });
-    }
-
-    private createPostCssLoaderUse(isProduction: boolean): RuleSetUseItem {
-        return {
-            loader: 'postcss-loader',
-            options: {
-                ident: 'postcss',
-                sourceMap: true,
-                plugins: () => {
-                    const autoprefixer = require('autoprefixer');
-
-                    const plugins = [
-                        autoprefixer({
-                            remove: false,
-                        }),
-                    ];
-
-                    if (isProduction) {
-                        const cssNano = require('cssnano');
-
-                        plugins.push(cssNano);
-                    }
-
-                    return plugins;
-                },
-            },
-        };
     }
 }
---
+++ lib/utility/webpackConfigHelper.ts
@@ -0,0 +1,16 @@
+import { Plugin, Configuration } from 'webpack';
+
+export function checkPluginInstanceIsInWebpackConfig(
+    plugin: Function,
+    webpackConfig: Configuration
+): boolean {
+    if (typeof webpackConfig.plugins === 'undefined') {
+        return false;
+    }
+
+    const plugins = webpackConfig.plugins;
+
+    return (
+        plugins.findIndex(cursorPlugin => cursorPlugin instanceof plugin) !== -1
+    );
+}
