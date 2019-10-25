2c5446fe36b9674cc11f5db91575ed450fa3e679
Gijs Nieuwenhuis <gijs.nieuwenhuis@freshheads.com>
2019-10-25 15:14:50 +0200

    #39 extend module helper with functionality to check if modules exists with a minimum version constraint

--- lib/utility/moduleHelper.ts
+++ lib/utility/moduleHelper.ts
@@ -1,7 +1,55 @@
-import detectInstalled from 'detect-installed';
+import { execSync } from 'child_process';
+import { gte as versionIsGreaterThanOrEqualTo } from 'semver';
 
-export function checkIfModuleIsInstalled(module: string): boolean {
-    return detectInstalled.sync(module, {
-        local: true,
+type TInstalledModules = {
+    [key: string]: string;
+};
+
+export function checkIfModuleIsInstalled(
+    module: string,
+    minVersion?: string
+): boolean {
+    const installedModules = resolveListOfInstalledRootModules();
+
+    const moduleIsInstalled = typeof installedModules[module] !== 'undefined';
+
+    if (!moduleIsInstalled) {
+        return false;
+    }
+
+    if (minVersion) {
+        const installedVersion = installedModules[module];
+
+        return versionIsGreaterThanOrEqualTo(installedVersion, minVersion);
+    }
+
+    return true;
+}
+
+let inMemoryCache: TInstalledModules | null = null;
+
+function resolveListOfInstalledRootModules() {
+    if (inMemoryCache) {
+        return inMemoryCache;
+    }
+
+    const stdout = execSync('npm list --json --depth=0');
+
+    const outputAsJson = JSON.parse(stdout.toString());
+
+    const dependencies = outputAsJson.dependencies;
+
+    if (!dependencies) {
+        throw new Error('Could not extract dependencies from stdout');
+    }
+
+    const filteredOutput: { [key: string]: string } = {};
+
+    Object.keys(dependencies).forEach(module => {
+        filteredOutput[module] = dependencies[module].version;
     });
+
+    inMemoryCache = filteredOutput;
+
+    return filteredOutput;
 }
