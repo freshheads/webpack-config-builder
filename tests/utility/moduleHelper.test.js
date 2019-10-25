8b549fa36c7cd8655fc4273d701e9876ae41ffa9
Gijs Nieuwenhuis <gijs.nieuwenhuis@freshheads.com>
2019-10-25 15:14:50 +0200

    #39 extend module helper with functionality to check if modules exists with a minimum version constraint

---
+++ tests/utility/moduleHelper.test.js
@@ -0,0 +1,60 @@
+const {
+    checkIfModuleIsInstalled,
+} = require('./../../build/utility/moduleHelper');
+
+describe('moduleHelper', () => {
+    describe('::checkIfModuleIsInstalled()', () => {
+        describe('When applied on an installed module', () => {
+            describe('..without a version', () => {
+                it('returns true', () => {
+                    const result = checkIfModuleIsInstalled('webpack');
+
+                    expect(result).toBe(true);
+                });
+            });
+
+            describe('..with a version', () => {
+                describe('..that is installed exactly', () => {
+                    it('returns true', () => {
+                        const result = checkIfModuleIsInstalled(
+                            'webpack',
+                            '4.39.1'
+                        );
+
+                        expect(result).toBe(true);
+                    });
+                });
+
+                describe('..that is above the minimum required', () => {
+                    it('returns true', () => {
+                        const result = checkIfModuleIsInstalled(
+                            'webpack',
+                            '4.38.0'
+                        );
+
+                        expect(result).toBe(true);
+                    });
+                });
+
+                describe('..that is below the minimum required', () => {
+                    it('returns false', () => {
+                        const result = checkIfModuleIsInstalled(
+                            'webpack',
+                            '155.12.0'
+                        );
+
+                        expect(result).toBe(false);
+                    });
+                });
+            });
+        });
+
+        describe('When applied on an not-installed module', () => {
+            it('returns false', () => {
+                const result = checkIfModuleIsInstalled('non-existant');
+
+                expect(result).toBe(false);
+            });
+        });
+    });
+});
