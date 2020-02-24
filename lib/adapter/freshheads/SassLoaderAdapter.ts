2cc9a0f81f11717c4510ea4ab489cda1344ea6ae
yannick van bladel <yannick.van.bladel@freshheads.com>
2020-02-24 15:13:28 +0100

    add comments clarifying implementation

--- lib/adapter/freshheads/SassLoaderAdapter.ts
+++ lib/adapter/freshheads/SassLoaderAdapter.ts
@@ -40,7 +40,10 @@ export default class SassLoaderAdapter implements Adapter {
         next: NextCallback
     ) {
         this.validateAllRequiredModulesAreInstalled();
-        this.config.sassLoaderOptions.implementation = require('sass'); 
+        // add implementation here instead of directly in DEFAULT_CONFIG. 
+        // If added in DEFAULT_CONFIG the require tries to import sass even though sass is disabled.
+        // This causes problems when sass isn't installed.
+        this.config.sassLoaderOptions.implementation = require('sass'); // prefer `dart-sass` instead of `node-sass` in case both are installed
 
         if (typeof webpackConfig.module === 'undefined') {
             webpackConfig.module = { rules: [] };
