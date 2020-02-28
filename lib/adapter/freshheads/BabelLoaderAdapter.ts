07ea1713f32359b497a284ba78b9ccae0b05848c
Martin Broos <martin.broos@freshheads.com>
2020-02-28 15:18:36 +0100

    require babel-loader 8.0.6

--- lib/adapter/freshheads/BabelLoaderAdapter.ts
+++ lib/adapter/freshheads/BabelLoaderAdapter.ts
@@ -61,7 +61,7 @@ export default class BabelLoaderAdapter implements Adapter {
 
     private validateAllRequiredModulesAreInstalled() {
         const requiredModules = {
-            'babel-loader': '8.0.0',
+            'babel-loader': '8.0.6',
             '@babel/preset-env': '7.8.0',
         };
 
