7d7a1f5ec216ab7ebc73a0ee03ebcfaa942ef3aa
Martin Broos <martin.broos@freshheads.com>
2020-06-18 11:12:17 +0200

    correct sass tests inside css adapter

--- tests/adapter/freshheads/CssAdapter.test.js
+++ tests/adapter/freshheads/CssAdapter.test.js
@@ -179,22 +179,24 @@ describe('FreshheadsCssAdapter', () => {
     });
 
     describe('With sass enabled', () => {
-        const config = {
-            sass: {
-                enabled: true,
-            },
-        };
+        it('should add the correct test statements', () => {
+            const config = {
+                sass: {
+                    enabled: true,
+                },
+            };
 
-        const adapter = new FreshheadsCssAdapter(config);
-        const webpackConfig = {};
+            const adapter = new FreshheadsCssAdapter(config);
+            const webpackConfig = {};
 
-        adapter.apply(webpackConfig, { env: 'dev' }, () => {});
+            adapter.apply(webpackConfig, { env: 'dev' }, () => {});
 
-        // rule two is added when Sass Loader Adapter gets added
-        const lastTwoRules = webpackConfig.module.rules.slice(-2);
+            // rule two is added when Sass Loader Adapter gets added
+            const lastTwoRules = webpackConfig.module.rules.slice(-2);
 
-        expect(lastTwoRules[0].test).toEqual(/\.s?css$/);
-        expect(lastTwoRules[1].test).toEqual(/\.scss$/);
+            expect(lastTwoRules[0].test).toEqual(/\.s?css$/);
+            expect(lastTwoRules[1].test).toEqual(/\.scss$/);
+        });
     });
 
     describe('When done', () => {
