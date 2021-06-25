da239f3f04a3752a1473ea4bddc9aed5384db9df
Gijs Nieuwenhuis <gijs.nieuwenhuis@freshheads.com>
2021-06-25 15:01:32 +0200

    #106 Updated command to list installed dependencies to check against builder requirements, to work with NPM 7
    * Only check package-lock.json, not actual node modules, as node modules now might be stored in the root project, when using workspaces
    * Check in both dev and prod modules, as it does not matter where the dependency is installed, as long as it is there

--- lib/utility/moduleHelper.ts
+++ lib/utility/moduleHelper.ts
@@ -48,8 +48,9 @@ function resolveListOfInstalledRootModules(): TInstalledModules {
         return inMemoryCache;
     }
 
-    // gets all dev dependencies from package.json,
-    const command = 'npm list --json --dev=true';
+    // gets all dependencies (incl. dev) from package-lock.json, not from node_modules folder, as with NPM 7+ the node
+    // modules might be stored in the root project node_modules folder and not in a child-project
+    const command = 'npm list --json --package-lock-only';
     let commandOutput: string | null;
 
     try {
